5b144b9a0182d30d9cef3079114610e64628c47a
julianperezm <julian.perezmunoz96@gmail.com>
2020-03-13 20:10:36 +0100

    update js03

--- practica03/js03.js
+++ practica03/js03.js
@@ -17,41 +17,60 @@ function countof(cad, type) {
      return contar
 }
 function psswd(pswd, longtot, minminus, minmayus, minnum, mincarac, specialcarac){
-    if (pswd.length === longtot) {
-        if (countof(pswd, minusculas) >= minminus){
-            if (countof(pswd, mayusculas) >= minmayus){
-                if (countof(pswd, numeros) >= minnum){
-                    if (countof(pswd, specialcarac) >= mincarac) {
-                        return "ok"
-                    }else {
-                        return "Error: Numero minimo de caracteres espciales"
-                    }
-
+    if (isNaN(longtot)) {
+        return "Error: La longitud dada no es un número";
+    }else{
+        if (pswd.length === longtot) {
+                if (isNaN(minminus)) {
+                        return "Error: El numero minumo de minusculas no es un numero"
                 }else {
-                    return "Error: Numero minimo de números"
+                    if (countof(pswd, minusculas) >= minminus){
+                        if (isNaN(minmayus)) {
+                            return "Error: El numero minumo de mayusculas no es un numero"
+                        }else {
+                            if (countof(pswd, mayusculas) >= minmayus) {
+                                if (isNaN(minnum)) {
+                                    return "Error: El numero minumo de numeros no es un numero"
+                                }else {
+                                    if (countof(pswd, numeros) >= minnum) {
+                                        if (isNaN(mincarac)) {
+                                            return "Error: El numero minumo de caracteres especiales no es un numero"
+                                        }else {
+                                            if (countof(pswd, specialcarac) >= mincarac) {
+                                                return "ok"
+                                            }else {
+                                                return "Error: Numero minimo de caracteres espciales"
+                                            }
+                                        }
+                                    }else {
+                                        return "Error: Numero minimo de números"
+                                    }
+                                }
+                            }else {
+                                return "Error: Numero minimo de mayusculas exigido"
+                            }
+                        }
+                    }else {
+                        return "Error: Numero minimo de minusculas exigido"
+                    }
                 }
-            }else {
-                return "Error: Numero minimo de mayusuclas "
-            }
         }else {
-            return "Erro: Numero minimo de minusculas exigido"
+            return "Error: longitud incorrecta"
         }
-
-    }else {
-        return "Error:longitud incorrecta"
     }
+
 }
 
 let pswd, longtot
 
 pswd = 'holaAA![]¿'
-longtot = 11
+longtot = 10
 var especial = ",.-{}[]!·$%&/()=?¿¡'"
-console.log(psswd(pswd,longtot,0,1,0,5,especial));
+console.log(psswd(pswd,longtot,0,1,0,4,especial));
 
 pswd = 'holaAA1'
 longtot = 7
-console.log(psswd(pswd,longtot,0,1,2,0,especial));
+console.log(psswd(pswd,longtot,0,'hola',2,0,especial));
 
 pswd = 'holaAA1234'
 longtot = 10
@@ -64,3 +83,7 @@ console.log(psswd(pswd,longtot,6,1,0,0,especial));
 pswd = 'hola'
 longtot = 4
 console.log(psswd(pswd,longtot,0, 3,0,0,especial));
+
+pswd = 'hola'
+longtot = 4
+console.log(psswd(pswd,longtot,0, 0,0,"hola",especial));
